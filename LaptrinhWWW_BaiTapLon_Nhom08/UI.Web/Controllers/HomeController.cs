862d344d30fe09228943f1d0a31f4b96b5773dd5
duytan2030 <tantuoiti2030@gmail.com>
2020-07-01 10:43:31 +0700

    abc

--- LaptrinhWWW_BaiTapLon_Nhom08/UI.Web/Controllers/HomeController.cs
+++ LaptrinhWWW_BaiTapLon_Nhom08/UI.Web/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Entities;
+using Services;
 
 namespace UI.Web.Controllers
 {
     public class HomeController : Controller
     {
+        NewspaperService serNews = new NewspaperService();
         public ActionResult Index()
         {
-            return View();
+            var list = serNews.GetAll();
+            List<eNewspaper> lst = new List<eNewspaper>();
+            foreach (var item in list)
+            {
+                lst.Add(new eNewspaper() { Active = item.Active, Description = item.Description, Image = item.Image, Journalist = item.Journalist, NewsId = item.NewsId, PublicationDate = (DateTime)item.PublicationDate, Title = item.Title });
+            }
+            return View(lst);
         }
 
         public ActionResult About()
@@ -26,5 +35,6 @@ namespace UI.Web.Controllers
 
             return View();
         }
+
     }
 }
