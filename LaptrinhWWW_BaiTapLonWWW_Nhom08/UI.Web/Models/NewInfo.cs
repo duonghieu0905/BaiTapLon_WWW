e35144d287b3a89eade036b8b76a12e9fcccf813
= <=duongchihieu.090599@gmail.com>
2020-07-06 22:55:12 +0700

    hieu commit v3

---
+++ LaptrinhWWW_BaiTapLonWWW_Nhom08/UI.Web/Models/NewInfo.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameworks.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Web.Models
+{
+    public class NewInfo
+    {
+        public int NewsId { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Journalist { get; set; }
+        public List<Topic> Topic { get; set; }
+           
+    }
+}
